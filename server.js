3059b68a14d93cf75bf54e96999e29b9da5c977a
blessingmad <mad22008@byui.edu>
2025-03-18 11:44:33 +0200

    Connect database

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require ('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3001;
 
 
-//app.use('/', require('/routes'));
+//app.use('/', require('./routes'));
 
-
+mongodb.intDb((err => {
+    if (err){
+        console.log;
+    }
+    else {
+        console.log(`Database is listening at and node running on port ${port}`);
+    }
+}));
 
 
 app.listen (port, () => {console.log(`Running on port ${port}`)});
