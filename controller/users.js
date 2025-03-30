99ce8ab38ad9a268c237a48952bc40c3cd918256
blessingmad <mad22008@byui.edu>
2025-03-31 00:04:04 +0200

    route update

--- controller/users.js
+++ controller/users.js
@@ -4,7 +4,7 @@ const ObjectId = require('mongodb').ObjectId;
 const getAll = async (req, res) => {
     //swagger.tag=['Users']
 
-    const result = await mongodb.getDatabase().db('Project1').collection('users').find();
+    const result = await mongodb.getDatabase ().collection('users').find();
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users);
@@ -18,7 +18,7 @@ const getSingle = async (req, res) => {
         return res.status(400).json({err:'Invalid id formart'});
     }
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db('Project1').collection('users').find( {_id: userId});
+    const result = await mongodb.getDatabase ().collection('users').find( {_id: userId});
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users[0]);
