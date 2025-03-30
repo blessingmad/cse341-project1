838e2fd0e1fe36819fc6304158a997cf4621302e
blessingmad <mad22008@byui.edu>
2025-03-31 00:15:24 +0200

    update.db

--- controller/users.js
+++ controller/users.js
@@ -35,7 +35,7 @@ const createUser = async (req, res) => {
         favoriteColor: req.body.favoriteColor,
         birthday: req.body.birthday
     };
-    const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
+    const response = await mongodb.getDatabase().collection('users').insertOne(user);
     if (response.acknowledged) {
         res.status(204).send();
     } 
